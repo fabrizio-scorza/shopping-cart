962347064804ce7bf17918ab74dc569358f42c9c
Jasczlv <ianis.cozlov@gmail.com>
2024-03-22 10:26:01 +0100

    ADD css custom properties

---
+++ js/app.js
@@ -0,0 +1,2 @@
+// MILESTONE 1
+// Analizziamo il layout e cerchiamo di individurare delle variabili che potrebbero esserci utili per lo sviluppo del progetto: colori, spazi, bordi, larghezze, etc. Impostiamo quindi delle custom properties da sfruttare nel CSS.
