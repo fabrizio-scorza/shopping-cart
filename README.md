443cffe2bd82ddd966f299af23e6989dc33e11e9
AntoCic <anto.cic.127@gmail.com>
2024-03-22 12:44:52 +0100

    ADDED header section in main and aside

---
+++ README.md
@@ -0,0 +1,16 @@
+## MILESTONE 1
+
+Analizziamo il layout e cerchiamo di individurare delle variabili che potrebbero esserci utili per lo sviluppo del progetto: colori, spazi, bordi, larghezze, etc. Impostiamo quindi delle custom properties da sfruttare nel CSS.
+
+## Milestone 2
+
+Impostiamo il macro layout. Facciamo in modo che vengano affiancati i due box con la giusta proporzione nella larghezza e con la giusta centratura rispetto alla viewport.
+
+## Milestone 3
+
+Osservando il layout ragioniamo su come impostare i nostri dati per mostrarli all’utente. Proviamo a creare un oggetto che rappresenti uno dei prodotti in tabella: di quali proprietà abbiamo bisogno? che tipo di dato sono? Con quale struttura gestiamo la lista di prodotti? N.B. nello scaffolding abbiamo già gli asset di riferimento (le immagini).
+
+## Milestone 4
+
+Impostiamo gli header (titoli e linea di separazione) delle due sezioni. In particolare:
+il numero di items mostrati nella sezione di sinistra deve essere dinamico (se sto gestendo 10 prodotti deve apparire la scritta “10 items”)
--- js/app.js
+++ js/app.js
@@ -1,45 +1,37 @@
-// MILESTONE 1
-// Analizziamo il layout e cerchiamo di individurare delle variabili che potrebbero esserci utili per lo sviluppo del progetto: colori, spazi, bordi, larghezze, etc. Impostiamo quindi delle custom properties da sfruttare nel CSS.
-// Milestone 3
-// Osservando il layout ragioniamo su come impostare i nostri dati per mostrarli all’utente. Proviamo a creare un oggetto che rappresenti uno dei prodotti in tabella: di quali proprietà abbiamo bisogno? che tipo di dato sono? Con quale struttura gestiamo la lista di prodotti? N.B. nello scaffolding abbiamo già gli asset di riferimento (le immagini).
-
-
 const cartItems = [
-    {
-        quantity: 1,
-        product: {
-            name: 'Burger', //string
-            img: 'burger.png', //string
-            price: 8.50, //number
-        },
-    
+  {
+    quantity: 1,
+    product: {
+      name: "Burger", //string
+      img: "burger.png", //string
+      price: 8.5, //number
     },
-    {
-        quantity: 1,
-        product: {
-            name: 'French Fries', //string
-            img: 'french-fries.png', //string
-            price: 5.50, //number
-        },
-    
+  },
+  {
+    quantity: 1,
+    product: {
+      name: "French Fries", //string
+      img: "french-fries.png", //string
+      price: 5.5, //number
     },
-    {
-        quantity: 1,
-        product: {
-            name: 'Kebab', //string
-            img: 'kebab.png', //string
-            price: 7, //number
-        },
-    
+  },
+  {
+    quantity: 1,
+    product: {
+      name: "Kebab", //string
+      img: "kebab.png", //string
+      price: 7, //number
     },
-    {
-        quantity: 1,
-        product: {
-            name: 'Pizza', //string
-            img: 'pizza.png', //string
-            price: 10, //number
-        },
-    
-    }
-]
-    
+  },
+  {
+    quantity: 1,
+    product: {
+      name: "Pizza", //string
+      img: "pizza.png", //string
+      price: 10, //number
+    },
+  },
+];
+
+const qItems = document.getElementById("q-items");
+qItems.textContent = cartItems.length + " items";
